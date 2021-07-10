9c8bbc2ddc365ab6bedfc35a7b74b81dd1acbcdd
Minjun Kim <public.velopert@gmail.com>
2021-07-10 22:44:53 +0900

    Create directories for each examples

--- packages/mooyaho-client-sample/p2p-channel/main.ts
+++ packages/mooyaho-client-sample/p2p-channel/main.ts
@@ -1,5 +1,8 @@
 import Mooyaho, { MooyahoConfig } from 'mooyaho-client-sdk'
 
 const config: MooyahoConfig = {
-  url: 'ws://localhost:8081/',
+  url: 'ws://localhost:8081',
 }
+
+console.log(config)
+const mooyaho = new Mooyaho(config)
